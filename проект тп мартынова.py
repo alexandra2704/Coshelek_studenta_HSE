f85d9f666b593981db309a9ff47785bd505f51f9
Mrmrtnv <mrmrtnv12@gmail.com>
2024-10-23 11:55:09 +0300

    Update проект тп мартынова.py

--- проект тп мартынова.py
+++ проект тп мартынова.py
@@ -8,3 +8,18 @@ def validateAmount(amountStr):
     return 0
 
 '''
+'''
+def process_category_step(message):
+    try:
+        cid = message.chat.id
+        cat_text = message.text
+        if not cat_text in CATEGORIES:
+            raise Exception("Извините, я не узнаю эту категорию \"{}\"!".format(cat_text))
+
+        choice[cid] = cat_text
+        message = bot.send_message(cid, 'Сколько ты потратил на {}? \n(Введи только число)'.format(str(choice[cid])))
+        bot.register_next_step_handler(message, process_amount_step)
+    except Exception as e:
+        bot.reply_to(message, 'Упсссс! ' + str(e))
+
+'''
