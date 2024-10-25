ea4a4d6dc646b41785626c7849aa5acae4a7811b
Daniiiiiiiiiiiiiiiiiiil <afanasevdaniil28@gmail.com>
2024-10-25 13:33:58 +0300

    feedback

--- by afanasyeva.py
+++ by afanasyeva.py
@@ -5,3 +5,11 @@ def addUserHistory(cid, recordText):
 
     global_users_dict[str(cid)].apend(recordText)
     return global_users_dict
+
+def process_feed_back(message):
+    cid = message.chat.id
+    feedback_text = message.text
+    print("****************ОБРАТНАЯ СВЯЗЬ********************")
+    print("chatid:{} feedback: {}".format(str(cid), feedback_text))
+    print("*********************************************")
+    bot.send_message(cid, 'СПАСИБО ЗА ОТЗЫВ!')
